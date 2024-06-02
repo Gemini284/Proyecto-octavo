e752bc1278a8b1ce920b0061f9551bee2549d585
Vicente Javier Viera Guízar <plantedcoot706@gmail.com>
2024-06-01 20:09:11 -0600

    Fixed script path bug

--- Backend/src/controllers/rest/AnalizeCtrl.ts
+++ Backend/src/controllers/rest/AnalizeCtrl.ts
@@ -18,36 +18,38 @@ class AnalizeCtrl {
     constructor(private screenshotService: Screenshot) {};
 
     @Post()
-    async Analize(@BodyParams() @Required() page: Page, @Res() res: Response): Promise<Buffer> {
+    async Analize(@BodyParams() @Required() page: Page, @Res() res: Response): Promise<void> {
         try{
             const { url } = page;
             const screenshot: Buffer = await this.screenshotService.screenshot(url);
 
-            const SCRIPT_PATH: string = path.resolve(__dirname, "../scripts/test.py");
+            const SCRIPT_PATH: string = path.resolve(__dirname, "../../scripts/test.py");
             const PYTHON_PROCESS = spawn("python3", [SCRIPT_PATH, "ElPepe", "666", "--ciudad", "Tepic"]);
 
             const responseBuff: Buffer[] = [];
-            const bufferData: Buffer = await new Promise((resolve, reject) => {
-                try{
-                    PYTHON_PROCESS.stdout.on("data", resBuffer => responseBuff.push(resBuffer));
-                    PYTHON_PROCESS.stdout.on("end", () => {
-                        const RESULT_PNG: Buffer = Buffer.concat(responseBuff);
-                        // res.contentType("image/png");
-                        resolve(RESULT_PNG);
-                    });
-                }catch(error){
-                    reject(error);
-                }
+            const bufferDataPromise: Promise<Buffer> = new Promise<Buffer>((resolve, reject) => {
+                PYTHON_PROCESS.stdout.on("data", resBuffer => {
+                    console.log("Received data chunk from Python script: ", resBuffer);
+                    responseBuff.push(resBuffer)
+                });
+                PYTHON_PROCESS.stdout.on("end", () => {
+                    const RESULT_PNG: Buffer = Buffer.concat(responseBuff);
+                    console.log("Buffer Lenght: ", RESULT_PNG.length);
+                    // res.contentType("image/png");
+                    resolve(RESULT_PNG);
+                });
+
+                // Handle errors
+                PYTHON_PROCESS.stderr.on("data", data => reject(data?.toString()));
+                PYTHON_PROCESS.on("close", code => code !== 0 ? reject(new Error("Error executing Python process.")) : null);
             });
 
-            // Handle errors
-            PYTHON_PROCESS.stderr.on("data", data => console.log("Error in data: ", data));
-            PYTHON_PROCESS.on("close", code => code !== 0 ? res.status(500).send("Error executing Python process.") : null);
-        
-            return bufferData;
+            const bufferData: Buffer = await bufferDataPromise;
+            //res.contentType("text");
+            res.send(bufferData);
         }catch(error){
-            res.status(500).send(error.message);
-            throw new Error(error);
+            console.error("Error in AnalizeCtrl: ", error);
+            if(!res.headersSent) res.status(500).send(error?.message);
         }
     }
 };
